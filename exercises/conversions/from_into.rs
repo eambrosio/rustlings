c6f84d9685e3afa7ffb3f9712248847b07c5d6fd
Emilio Ambrosio <emilio.ambrosio@joyn.de>
2024-06-27 16:07:29 +0200

    add conversions

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,28 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        match s.len() {
+            0 => Person::default(),
+            _ => {
+                let person: Vec<&str> = s.split(",").collect();
+                match person.len() {
+                    n if n > 1 => match person[0] {
+                        "" => Person::default(),
+                        name => match person[1].parse::<usize>() {
+                            Ok(age) => Person {
+                                name: name.to_owned(),
+                                age,
+                            },
+                            Err(_) => Person::default(),
+                        },
+                    },
+
+                    _ => Person::default(),
+                }
+            }
+        }
     }
 }
 
--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -31,8 +31,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
-
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
 // 2. Split the given string on the commas present in it
@@ -52,6 +50,25 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        match s {
+            "" => Err(ParsePersonError::Empty),
+            s => {
+                let person: Vec<&str> = s.split(",").collect();
+                match person.len() {
+                    2 => match person[0] {
+                        "" => Err(ParsePersonError::NoName),
+                        name => match person[1].parse::<usize>() {
+                            Err(e) => Err(ParsePersonError::ParseInt(e)),
+                            Ok(age) => Ok(Person {
+                                name: name.to_owned(),
+                                age,
+                            }),
+                        },
+                    },
+                    _ => Err(ParsePersonError::BadLen),
+                }
+            }
+        }
     }
 }
 
